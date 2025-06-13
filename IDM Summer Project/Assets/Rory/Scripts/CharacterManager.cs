11354f624bd2ee5064a9a567e59bbe7fe16c6bda
AxelfTV <axelf196@gmail.com>
2025-06-13 11:08:12 +0100

    Added Cinemachine

--- IDM Summer Project/Assets/Rory/Scripts/CharacterManager.cs
+++ IDM Summer Project/Assets/Rory/Scripts/CharacterManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class CharacterController : MonoBehaviour
+public class CharacterManager : MonoBehaviour
 {
     
     InputHandler input;
