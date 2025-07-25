8c82a31295c61c0aa8800f2fece79b63689bc074
AxelfTV <axelf196@gmail.com>
2025-07-25 10:35:12 +0100

    Organisation

--- IDM Summer Project/Assets/Rory/Scripts/Camera/SpiralCam.cs
+++ IDM Summer Project/Assets/Rory/Scripts/Camera/SpiralCam.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BeanstalkCam : MonoBehaviour
+public class SpiralCam : MonoBehaviour
 {
 
     [SerializeField] CinemachineVirtualCamera cam;
